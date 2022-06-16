efd13f2eebefef1ff53ed8c4bcfcaea94e25f73a
TheoAcker12 <theo.o.acker-1@ou.edu>
2022-06-16 10:48:37 -0500

    fix scrollama problems

--- assets/tour.js
+++ assets/tour.js
@@ -429,17 +429,15 @@ if ($("#scrolly .step").length > 0) {
         step: "#scrolly .step",
         offset: SCROLLAMA_OFFSET,
         debug: SCROLLAMA_DEBUG,
+        container: document.getElementById("tour-wrapper"),
     }).onStepEnter(function(e) {
-        console.log('step enter: ' + e.element.id);
         if (scrolly_wait) {
             scrolly_wait--;
         }
         else if (!isMobile() && tour_state.animation) {
-            scrolly_temp_view = e.element.id;
-            console.log('scrolled to view');
+            scrolly_temp_view = e.element.getAttribute("data-view");
             // use timeout function so that if multiple views are scrolled through at once, only the last view will be truly entered
             setTimeout(function() {
-                console.log('entering view');
                 enterView(scrolly_temp_view);
             }, SCROLLAMA_ENTER_VIEW_WAIT);
         }
