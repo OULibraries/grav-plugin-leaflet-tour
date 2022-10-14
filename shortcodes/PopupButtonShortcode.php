4d8371c33bb0102d5c883c913a5585293a3881fc
TheoAcker12 <theo.o.acker-1@ou.edu>
2022-10-14 17:33:38 -0500

    update shortcode for php compatibility

--- shortcodes/PopupButtonShortcode.php
+++ shortcodes/PopupButtonShortcode.php
@@ -3,6 +3,7 @@
 namespace Grav\Plugin\Shortcodes;
 
 use Grav\Plugin\LeafletTour\LeafletTour;
+use Grav\Plugin\LeafletTour\Utils;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 class PopupButtonShortcode extends Shortcode {
@@ -11,11 +12,11 @@ class PopupButtonShortcode extends Shortcode {
         $this->shortcode->getHandlers()->add('popup-button', function(ShortcodeInterface $sc) {
             // get the tour
             $page = $this->grav['page'];
-            if (($page->template() === 'tour') && ($tour = LeafletTour::getTour(((array)$page->header())['id']))) {
+            if (($page->template() === 'tour') && ($tour = LeafletTour::getTour(Utils::get(((array)$page->header()), 'id')))) {
                 // make sure the feature is in the tour and has popup content
                 $id = $sc->getParameter('id');
                 $popups = $tour->getFeaturePopups();
-                if ($popups[$id] && ($name = $popups[$id]['name'])) {
+                if (Utils::get($popups, $id) && ($name = Utils::get($popups[$id], 'name'))) {
                     // get the button
                     return LeafletTour::buildPopupButton($id, 'sc_btn_' . $this->shortcode->getId($sc), $name, $sc->getContent());
                 }
